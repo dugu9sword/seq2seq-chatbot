f6e28b85bb373b666527f1d26201b009c53703ae
dugu9sword <dugu9sword@163.com>
2017-04-29 14:56:22 +0800

    robust

--- test_seq2seq.py
+++ test_seq2seq.py
@@ -1,38 +1,41 @@
-from n2nds.data import Data
-from n2nds.config import Config
-from n2nds.reader import WeiboReader
 import tensorflow as tf
+import os
+from n2nds.reader import WeiboReader
 from n2nds.seq2seq import Model
 
 
 def main():
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
+
+    # Load the data set
     train_weibo = WeiboReader("dataset/stc_weibo_train_post_generated_10",
                               "dataset/stc_weibo_train_response_generated_10",
                               batch_size=10)
 
+    # Create the model
     with tf.name_scope("Train"):
-        with tf.variable_scope("Model", initializer=tf.random_uniform_initializer(-0.01, 0.01)) as scope:
+        with tf.variable_scope("Model", initializer=tf.random_uniform_initializer(-0.01, 0.01)):
             train_model = Model(train_weibo.config, is_train=True)
 
     with tf.name_scope("Valid"):
         with tf.variable_scope("Model", reuse=True):
             valid_model = Model(train_weibo.config, is_train=False)
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    # Start the supervised session
+    sv = tf.train.Supervisor(logdir="tmp/logdir/",
+                             save_model_secs=2,
+                             save_summaries_secs=1,
+                             summary_op=None)
+    with sv.managed_session() as sess:
         # sum_writer = tf.summary.FileWriter('summary/sum', sess.graph)
+
         iter = 0
         while True:
             iter += 1
 
             data = train_weibo.next_batch()
-            # init_enc = sess.run(train_model.initial_enc_state)
-            # init_dec = sess.run(train_model.initial_dec_state)
             feed_dict = {}
-            # feed_dict[train_model.initial_enc_state.c] = init_enc.c
-            # feed_dict[train_model.initial_enc_state.h] = init_enc.h
-            # feed_dict[train_model.initial_dec_state.c] = init_dec.c
-            # feed_dict[train_model.initial_dec_state.h] = init_dec.h
             feed_dict[train_model.utter_indices] = data.indices
             feed_dict[train_model.utter_lengths] = data.lengths
             feed_dict[train_model.utter_weights] = data.weights
@@ -40,53 +43,33 @@ def main():
             sess.run(train_model.minimizier, feed_dict)
             if iter % 50 == 0:
                 cost, merged = sess.run([train_model.cost, train_model.merged], feed_dict)
+                sv.summary_computed(sess, summary=merged, global_step=iter)
                 # sum_writer.add_summary(merged, global_step=iter / 50)
                 print("iter %d : cost %f" % (iter // 50, cost))
-                if cost < 0.9:
+                if cost < 0.1:
                     break
 
-        print("Train model")
-        data = train_weibo.next_batch()
-        # init_enc = sess.run(train_model.initial_enc_state)
-        # init_dec = sess.run(train_model.initial_dec_state)
-        feed_dict = {}
-        # feed_dict[train_model.initial_enc_state.c] = init_enc.c
-        # feed_dict[train_model.initial_enc_state.h] = init_enc.h
-        # feed_dict[train_model.initial_dec_state.c] = init_dec.c
-        # feed_dict[train_model.initial_dec_state.h] = init_dec.h
-        feed_dict[train_model.utter_indices] = data.indices
-        feed_dict[train_model.utter_lengths] = data.lengths
-        feed_dict[train_model.utter_weights] = data.weights
-        pred = sess.run([train_model.pred], feed_dict)
-        pred = pred[0].tolist()
-        print(pred)
-        i = 0
-        while True:
-            print(train_weibo.gen_words_from_indices(pred[i:i + train_weibo.config.SEQ_SIZE]))
-            i += train_weibo.config.SEQ_SIZE
-            if i >= len(pred):
-                break
 
-                # print("Valid model")
-                #
-                # init_enc = sess.run(valid_model.initial_enc_state)
-                # init_dec = sess.run(valid_model.initial_dec_state)
-                # feed_dict = {}
-                # feed_dict[valid_model.initial_enc_state.c] = init_enc.c
-                # feed_dict[valid_model.initial_enc_state.h] = init_enc.h
-                # feed_dict[valid_model.initial_dec_state.c] = init_dec.c
-                # feed_dict[valid_model.initial_dec_state.h] = init_dec.h
-                #
-                # pred= sess.run(valid_model.pred, feed_dict)
-                # pred = pred.tolist()
-                # print(pred)
-                #
-                # i = 0
-                # while True:
-                #     print(train_weibo.gen_words_from_indices(pred[i:i + config.SEQ_SIZE]))
-                #     i += config.SEQ_SIZE
-                #     if i >= len(pred):
-                #         break
+        train_output = open("tmp/train_output.txt", "w")
+        valid_output = open("tmp/valid_output.txt", "w")
+        for model, output in list(zip([train_model, valid_model],[train_output, valid_output])):
+            data = train_weibo.next_batch()
+            feed_dict = {}
+            feed_dict[model.utter_indices] = data.indices
+            feed_dict[model.utter_lengths] = data.lengths
+            feed_dict[model.utter_weights] = data.weights
+            pred = sess.run(model.pred, feed_dict)
+            pred = pred.tolist()
+            i = 0
+            while True:
+                pred_res = train_weibo.gen_words_from_indices(pred[i:i + train_weibo.config.SEQ_SIZE])
+                print(pred_res)
+                output.write(pred_res)
+                output.write("\n")
+                i += train_weibo.config.SEQ_SIZE
+                if i >= len(pred):
+                    break
+
 
 
 if __name__ == '__main__':
